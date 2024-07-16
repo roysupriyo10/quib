4c98b755619319926e3c5a4904048085cb17ee99
Supriyo Roy <roysupriyo10@gmail.com>
2024-07-17 03:29:11 +0530

    added redirection

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,7 +1,10 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <main>
-      <h1>Home page</h1>
-    </main>
-  );
+  redirect("/sign-in");
+  // return (
+  //   <main>
+  //     <h1>Home page</h1>
+  //   </main>
+  // );
 }
